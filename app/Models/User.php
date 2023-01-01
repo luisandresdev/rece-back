8c4f3e48ebfc8439e7365a71383d28f51f1057fe
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-01 20:11:51 +0100

    relaciona tags

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Category::class);
     }
+
+    public function tags():HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
