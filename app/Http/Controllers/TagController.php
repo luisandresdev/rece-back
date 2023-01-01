e052eb39f1cbdca8a5ba7f479c1bf06618ff270b
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-01 20:12:43 +0100

    controlador

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Response;
+use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['data' => Tag::get()], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTagRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTagRequest $request)
+    {
+        $data = $request->only(Tag::UPDATE_FIELDS);
+
+        $tag = Tag::create($data);
+
+        return response()->json(['data' => $tag], Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tag $tag)
+    {
+        return response()->json(['data' => $tag], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTagRequest  $request
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $data = $request->only(Tag::UPDATE_FIELDS);
+        $tag->update($data);
+        return response()->json(['data' => $tag], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        return response()->json(['data' => $tag], Response::HTTP_OK);
+    }
+}
