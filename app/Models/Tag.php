4f4b7ad08fd31645f0b59bf4d91ea1fde27e8818
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-01 20:12:26 +0100

    modelo

---
+++ app/Models/Tag.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUser;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Validation\Rule;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tag extends Model
+{
+    use HasFactory, HasUser;
+    protected $fillable = ['name', 'user_id'];
+
+    // update field
+    public const UPDATE_FIELDS = ['name', 'user_id'];
+
+    // relación
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    // rules
+    public static function rules($id = 0): array
+    {
+        return [
+            'name' => [
+                'required', 'string', Rule::unique('tags', 'name')->ignore($id, 'id'),
+            ],
+        ];
+    }
+}
