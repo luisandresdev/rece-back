9b02bf462fadb8341b8f6515a3675d349afe5695
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-01 18:57:59 +0100

    controlador

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Response;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['data' => Category::get()], Response::HTTP_OK);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $data = $request->only(Category::UPDATE_FIELDS);
+
+        $category = Category::create($data);
+
+        return response()->json(['data' => $category], Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return response()->json(['data' => $category], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $data = $request->only(Category::UPDATE_FIELDS);
+        $category->update($data);
+        return response()->json(['data' => $category], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json(['data' => $category], Response::HTTP_OK);
+    }
+}
