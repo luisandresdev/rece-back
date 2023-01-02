4c267f01a6b2e1f9967d00f62a0316790f80e62b
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-02 18:35:38 +0100

    controlador

---
+++ app/Http/Controllers/ShoppingListController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShoppingList;
+use Illuminate\Http\Response;
+use App\Http\Requests\StoreShoppingListRequest;
+use App\Http\Requests\UpdateShoppingListRequest;
+
+class ShoppingListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['data' => ShoppingList::get()], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreShoppingListRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreShoppingListRequest $request)
+    {
+        $data = $request->only(ShoppingList::UPDATE_FIELDS);
+
+        $shoppingList = ShoppingList::create($data);
+
+        return response()->json(['data' => $shoppingList], Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShoppingList $shoppingList)
+    {
+        return response()->json(['data' => $shoppingList], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateShoppingListRequest  $request
+     * @param  \App\Models\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateShoppingListRequest $request, ShoppingList $shoppingList)
+    {
+        $data = $request->only(ShoppingList::UPDATE_FIELDS);
+        $shoppingList->update($data);
+        return response()->json(['data' => $shoppingList], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShoppingList $shoppingList)
+    {
+        $shoppingList->delete();
+        return response()->json(['data' => $shoppingList], Response::HTTP_OK);
+    }
+}
