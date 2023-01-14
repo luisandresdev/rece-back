b246676f4edd73f3a732cea674f807d33604b79c
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-14 23:46:14 +0100

    relacion a productos

--- app/Models/ShoppingList.php
+++ app/Models/ShoppingList.php
@@ -7,27 +7,35 @@ use Illuminate\Validation\Rule;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ShoppingList extends Model
 {
     use HasFactory, HasUser, SoftDeletes;
     protected $fillable = ['name', 'user_id'];
 
-     // update field
-     public const UPDATE_FIELDS = ['name', 'user_id'];
+    // update field
+    public const UPDATE_FIELDS = ['name', 'user_id'];
 
-    
-     // rules
-     public static function rules($id = 0): array
-     {
-         return [
-             'name' => [
-                 'required', 'string', Rule::unique('shopping_lists')
-                     ->ignore($id, 'id,deleted_at,NULL')
-                     ->where(function ($query) { //Ignora por user_id
-                         $query->where('user_id', auth()->id());
-                     }),
-             ],
-         ];
-     }
+
+    // rules
+    public static function rules($id = 0): array
+    {
+        return [
+            'name' => [
+                'required', 'string', Rule::unique('shopping_lists')
+                    ->ignore($id, 'id,deleted_at,NULL')
+                    ->where(function ($query) { //Ignora por user_id
+                        $query->where('user_id', auth()->id());
+                    }),
+            ],
+        ];
+    }
+
+    // relaciones
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
