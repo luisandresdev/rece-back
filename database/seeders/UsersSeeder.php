335ecf7ccfd7ae80524390a80ac20f80a51c000a
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-15 16:41:45 +0100

    crear categorias y tags por defecto

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,10 +18,22 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $user = User::create([
             'name' => 'Luis Andrés Bolaños Yapo',
             'email' => 'luisandres33bolanos@gmail.com',
             'password' => Hash::make('SexPistols_1'),
         ]);
+
+        Category::upsert([
+            ['user_id' => $user->id, 'name' => 'Entrada'],
+            ['user_id' => $user->id, 'name' => 'Plato Principal'],
+            ['user_id' => $user->id, 'name' => 'Postre'],
+        ], ['user_id', 'name'], ['name']);
+
+        Tag::upsert([
+            ['user_id' => $user->id, 'name' => 'Rápido'],
+            ['user_id' => $user->id, 'name' => 'Rico'],
+            ['user_id' => $user->id, 'name' => 'Sano'],
+        ], ['user_id', 'name'], ['name']);
     }
 }
